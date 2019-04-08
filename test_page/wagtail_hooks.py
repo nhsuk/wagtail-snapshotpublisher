e65e98b398f0ec0b9a6a0d9110116b082c2d9015
Yohan Lebret <yohan.lebret@gmail.com>
2019-04-08 14:46:12 +0100

    Fix issue with custom edit modeladmin template

--- test_page/wagtail_hooks.py
+++ test_page/wagtail_hooks.py
@@ -6,6 +6,7 @@ from .models import TestModel
 class TestModelAdmin(ModelAdmin):
     model = TestModel
     menu_label = 'TestModel'
+    edit_template_name = 'modeladmin/edit_with_release.html'
 
 
 modeladmin_register(TestModelAdmin)
