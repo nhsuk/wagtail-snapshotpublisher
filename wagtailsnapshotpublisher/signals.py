047769d5a5ff6c29c8710f8f8a89768e2d719ba0
David Crees <david.a.crees@gmail.com>
2020-02-10 17:18:26 +0000

    Stripped special characters from paste op

--- wagtailsnapshotpublisher/signals.py
+++ wagtailsnapshotpublisher/signals.py
@@ -1,12 +1,12 @@
 import logging
-​
+
 from django import dispatch
 from django.dispatch import receiver
-​
+
 logger = logging.getLogger('django')
-​
+
 content_was_published = dispatch.Signal(providing_args=["site_id", "release_id", "title", "content"])
-​
+
 @receiver(content_was_published)
 def handle_content_was_published(sender, site_id, release_id, title, content, **kwargs):
-    logger.info('Content was published %s %s %s', site_id, release_id, title)
+  logger.info('Content was published %s %s %s', site_id, release_id, title)
