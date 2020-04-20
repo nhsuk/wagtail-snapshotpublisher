9f8d62a0cd3c99f5ef9a70254ad7b40f738beeb0
Richard da Silva <richard.dasilva1@nhs.net>
2020-04-20 11:41:19 +0100

    Removed content was published signal handling from wagtail snapshot publisher

--- wagtailsnapshotpublisher/signals.py
+++ wagtailsnapshotpublisher/signals.py
@@ -1,12 +1,7 @@
-import logging
-
 from django import dispatch
 from django.dispatch import receiver
+from django.conf import settings
 
-logger = logging.getLogger('django')
+from azure.storage.queue import QueueService
 
-content_was_published = dispatch.Signal(providing_args=["site_id", "release_id", "title", "content"])
-
-@receiver(content_was_published)
-def handle_content_was_published(sender, site_id, release_id, title, content, **kwargs):
-  logger.info('Content was published %s %s %s', site_id, release_id, title)
+content_was_published = dispatch.Signal(providing_args=["site_id", "release_id", "title", "content", "page"])
