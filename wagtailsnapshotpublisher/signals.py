87970bae40c1bc3e26ef3042c4536a52896f4cf4
Richard da Silva <richard.dasilva1@nhs.net>
2020-04-20 14:49:43 +0100

    Removed not needed libraries from signals

--- wagtailsnapshotpublisher/signals.py
+++ wagtailsnapshotpublisher/signals.py
@@ -1,7 +1,3 @@
 from django import dispatch
-from django.dispatch import receiver
-from django.conf import settings
-
-from azure.storage.queue import QueueService
 
 content_was_published = dispatch.Signal(providing_args=["site_id", "release_id", "title", "content", "page"])
